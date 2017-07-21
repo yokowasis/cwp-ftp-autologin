176ba8cf8c2550063f0fd7c2255587c3cb2220db
Wasis Haryo Sasoko <yokowasis@gmail.com>
2017-07-21 12:02:39 +0800

    Create ftp.php

---
+++ ftp.php
@@ -0,0 +1,16 @@
+<?php
+
+	$command = "pure-pw userdel $user_login-ftptemp;(echo ".$_SESSION['rkey']."; echo ".$_SESSION['rkey'].") | pure-pw useradd $user_login-ftptemp -d /home/sgbima/public_html/  -u  $user_login;pure-pw mkdb";
+
+	shell_exec($command);
+
+?>
+	<form method="POST" id="ftp_form" action="<?php echo ($htdocs_link) ?>/webftp_simple/">
+		<input type="hidden" name="login" value="1">		
+		<input type="hidden" name="ftp_user" value="<?php echo ("$user_login-ftptemp") ?>">		
+		<input type="hidden" name="ftp_pass" value="<?php echo ($_SESSION['rkey']) ?>">
+	</form>
+
+	<script type="text/javascript">
+		document.getElementById("ftp_form").submit();
+	</script>
